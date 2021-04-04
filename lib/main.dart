fa38ee7ba3310461f8dffcff8002bd15b2db82c3
Yevhenii Koshovets <y.koshovets@gmail.com>
2021-04-04 15:36:55 +0300

    Added text in the middle of the screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final double _fontSize = 30.0;
+  final String _welcomeText = "Hey there";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          _welcomeText,
+          style: TextStyle(fontSize: _fontSize),
+        )
+      )
+    );
+  }
+}
